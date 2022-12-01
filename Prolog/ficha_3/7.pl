2c0b0a1a2d65f8ef98a294fae7166b7b0e2a5081
Zé Diogo <zediogox@hotmail.com>
2022-12-01 16:25:53 +0000

    7 done

--- Prolog/ficha_3/7.pl
+++ Prolog/ficha_3/7.pl
@@ -12,3 +12,36 @@ class(fsi, t, '4 Qui', 12, 1).
 class(fsi, tp, '3 Qua', 8.5, 2).
 class(rc, t, '4 Qui', 8, 2).
 class(rc, tp, '5 Sex', 10.5, 2).
+
+% a 
+same_day(UC1, UC2) :- class(UC1,_,Day,_,_),class(UC2,_,Day,_,_).
+
+% b
+daily_courses(Day, Courses) :- findall(Course, class(Course,_,Day,_,_), Courses).
+
+% c
+short_classes(Courses) :- findall(Course/Type/Day, (class(Course,Type, Day, _, Dur), Dur < 2) , Courses).
+
+% d 
+
+course_classes(Course, Courses) :- findall(Day/Time-Type, class(Course, Type, Day, Time,_), Courses).
+
+% e
+
+courses(R) :- setof(Course, (CT,D,T,DT)^class(Course,CT,D,T,DT), R).
+
+% f
+
+print_list([]).
+print_list([H | T]) :- write(H), nl, print_list(T).
+
+% setof ordena (pela order de argumentos passados)
+schedule :- setof(Day/Time/Duration-Course-Type, class(Course, Type, Day, Time, Duration), Classes), print_list(Classes).
+
+
+% h
+
+find_class :- write('Please input the Day: '), read(Day), nl,
+              write('Please input the Time: '), read(Time), nl,
+              class(Course, _, Day, Time, Duration),
+              write(Course/Time/Duration).
