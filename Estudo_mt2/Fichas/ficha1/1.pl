9136ed0ceadfd51d62a222c079e2bb53632439c5
Zediogo96 <zediogox@hotmail.com>
2023-01-10 18:44:34 +0000

    ficha 1 ex1 done

--- Estudo_mt2/Fichas/ficha1/1.pl
+++ Estudo_mt2/Fichas/ficha1/1.pl
@@ -1,58 +1,101 @@
-female("Grace").
-female("DeDe").
-female("Gloria").
-female("Barb").
-female("Claire").
-female("Cameron").
-female("Bo").
-female("Haley").
-female("Lily").
-female("Poppy").
+% a)
+% gender
+female(grace).
+female(dede).
+female(gloria).
+female(barb).
+female(claire).
+female(pameron).
+female(haley).
+female(alex).
+female(lily).
+female(poppy).
 
-male("Rexford").
-male("George").
-male("Calhoun").
-male("Alex").
-male("Luke").
-male("Dylan").
-male("Pameron").
-male("Mitchell").
-male("Joe").
-male("Many").
-male("Phil").
-male("Javier").
-male("Merle").
-male("Jay").
-male("Frank").
+male(frank).
+male(jay).
+male(javier).
+male(merle).
+male(phil).
+male(mitchell).
+male(joe).
+male(manny).
+male(cameron).
+male(bob).
+male(dylan).
+male(luke).
+male(rexford).
+male(calhoun).
+male(george).
 
-% a is b's parent 
-parent("Grace","Phil").
-parent("Frank","Phil").
-parent("DeDe","Claire").
-parent("Jay","Claire").
-parent("Jay","Mitchell").
-parent("DeDe","Mitchell").
-parent("Jay","Joe").
-parent("Gloria","Joe").
-parent("Gloria","Many").
-parent("Javier","Many").
-parent("Barb","Cameron").
-parent("Merle","Cameron").
-parent("Barb","Pameron").
-parent("Merle","Pameron").
-parent("Phil","Haley").
-parent("Claire","Haley").
-parent("Phil","Alex").
-parent("Claire","Alex").
-parent("Phil","Luke").
-parent("Claire","Luke").
-parent("Mitchell","Lily").
-parent("Cameron","Lily").
-parent("Mitchell","Rexford").
-parent("Cameron","Rexford").
-parent("Bo","Calhoun").
-parent("Pameron","Calhoun").
-parent("Dylan","George").
-parent("Haley","George").
-parent("Dylan","Poppy").
-parent("Haley","Poppy").
+% relationships
+parent(grace, phil).
+parent(frank, phil).
+parent(dede, claire).
+parent(jay, claire).
+parent(dede, mitchell).
+parent(jay, mitchell).
+parent(jay, joe).
+parent(gloria, joe).
+parent(gloria, manny).
+parent(javier, manny).
+parent(barb, cameron).
+parent(merle, cameron).
+parent(barb, pameron).
+parent(merle, pameron).
+parent(phil, haley).
+parent(claire, haley).
+parent(phil, alex).
+parent(claire, alex).
+parent(phil, luke).
+parent(claire, luke).
+parent(mitchell, lily).
+parent(cameron, lily).
+parent(mitchell, rexford).
+parent(cameron, rexford).
+parent(pameron, calhoun).
+parent(bo, calhoun).
+parent(dylan, george).
+parent(haley, george).
+parent(dylan, poppy).
+parent(haley, poppy).
+
+% b
+    % i.    female(haley).
+    % ii.   male(gil).
+    % iii.  parent(frank, phil).
+    % iv.   parent(P, claire).
+    % v.    parent(gloria, S).
+    % vi.   parent(jay, _X), parent(_X, Y).
+    % vii.  parent(X, _Y), parent(_Y, lily).
+    % viii. parent(alex, _X). 
+    % ix.   parent(jay, X), \+ parent(gloria, X).    
+
+
+% c 
+    % a é pai de b
+    father(X,Y) :- parent(X,Y), male(X).
+    mother(X,Y) :- parent(X,Y), female(X).
+
+    grandparent(X,Y) :- father(X,_Z), parent(_Z,Y).
+    grandmother(X,Y) :- mother(X,_Z), parent(_Z,Y).
+
+    siblings(X,Y) :- parent(_Z,X), parent(_Z,Y), X \= Y.
+    % halfsiblings(X,Y) :- \+ siblings(X,Y), parent(P,X), parent(P,Y), (X \= Y).
+    cousins(X,Y) :- parent(PX, X), parent(PY, Y), siblings(PX, PY), (X \= Y), (PX \= PY).
+    uncle(X,Y) :- parent(P, Y), siblings(X,P).
+
+% d
+    % cousins(haley, lily).
+    % father(X, luke).
+    % uncle(X, lily).
+    % grandparent(X, lily).
+    % siblings(lily, S).
+    % halfsiblings(lily, HS).
+
+% e
+
+    married(jay, gloria, 2008).
+    married(jay, dede, 1968).
+    divorced(jay, dede, 2003).
+
+
