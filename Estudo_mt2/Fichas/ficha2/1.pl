4a6cea46267592b19c098b22bfa09f5cbafc0717
Zediogo96 <zediogox@hotmail.com>
2023-01-11 01:13:37 +0000

    ficha 2 ex 1

---
+++ Estudo_mt2/Fichas/ficha2/1.pl
@@ -0,0 +1,44 @@
+% 1
+
+% a
+
+fatorial(0,1).
+fatorial(N,F) :- 
+    NewN is N - 1,
+    fatorial(NewN, F1),
+    F is F1 * N.
+
+% b 
+
+somaRec(0,0).
+somaRec(N, Sum) :-
+    NewN is N - 1,
+    somaRec(NewN, Sum1),
+    Sum is Sum1 + N.
+
+% c
+
+fibonacci(0,0).
+fibonacci(1,1).
+fibonacci(N, Fib) :-
+    N1 is N - 1,
+    N2 is N - 2,
+    fibonacci(N1, Fib1),
+    fibonacci(N2, Fib2),
+    Fib is Fib1 + Fib2.
+
+% d
+
+
+isPrime(X) :- 
+    NX is X - 1,
+    isPrime(X,NX).
+
+isPrime(_,1) :- !.
+isPrime(X,Y) :-
+    (X mod Y) =\= 0,
+    NY is Y - 1,
+    isPrime(X, NY).
+
+
+
